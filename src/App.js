1d49625f4853f9c42413bad80aebfda953c9af4f
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-12-31 15:39:40 -0500

    Added some funtionality, working on getting posts to work

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React, { useEffect, useState } from "react";
-import logo from './logo.svg';
-import './App.css';
-import { Map, MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import { Icon } from 'leaflet';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Map,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { Icon } from "leaflet";
+import * as L from "leaflet";
 
-function MyComponent() {
-  const map = useMap()
-  console.log('map center:', map.getCenter())
-  return null
-}
+export const icon = new Icon({
+  iconUrl: "/bike-svgrepo-com.svg",
+  iconSize: [30, 30],
+});
 
-
-export default function App(){
-  
+export default function App() {
   const urlRacks = "http://localhost:8080/api/racks";
 
   const [racks, setRacks] = useState([]);
 
   const fetchDataRacks = () => {
     fetch(urlRacks)
-      .then(response => {
-        return response.json()
+      .then((response) => {
+        return response.json();
       })
-      .then(data => {
-        setRacks(data)
-      })
+      .then((data) => {
+        setRacks(data);
+      });
   };
 
   useEffect(() => {
-    fetchDataRacks()
-  }, [])
-
-  console.log(racks);
+    fetchDataRacks();
+  }, []);
 
   const urlReviews = "http://localhost:8080/api/reviews";
 
@@ -39,62 +43,106 @@ export default function App(){
 
   const fetchDataReviews = () => {
     fetch(urlReviews)
-      .then(response => {
-        return response.json()
+      .then((response) => {
+        return response.json();
       })
-      .then(data => {
-        setRacks(data)
-      })
+      .then((data) => {
+        setReviews(data);
+      });
   };
 
-  console.log(reviews);
+  useEffect(() => {
+    fetchDataReviews();
+  }, []);
 
   const [activeRack, setActiveRack] = React.useState(null);
 
-  console.log("active rack" +activeRack);
+  const [image, setImage] = React.useState(null);
 
-  return(
-<MapContainer center={[38.71666369749177, -75.07651347222257]} zoom={15} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
+  const imgSrc = "data:image/png;base64," + image;
 
-  {racks.map(rack => (
-    <Marker
-    key={rack.id}
-    position={[
-      rack.latitude, 
-      rack.longitude
-    ]}
-    eventHandlers={{
-      click: (e) => {
-        setActiveRack(rack)
-      }
-    }}
-    // onClick={() => {
-    //   setActiveRack(rack);
-    // }}
-    />
-  ))}
+  const [activeReview, setActiveReview] = React.useState(null);
 
-  {activeRack && (
-            <Popup
-              position={[
-                activeRack.latitude,
-                activeRack.longitude
-              ]}
-              onClose={() => {
-                setActiveRack(null);
-              }}
-            >
-              <div>
-                <p>{activeRack.name}</p>
-                {/* <img src="data:" + {rack.imageContentType} + "," + {rack.image} > */}
-              </div>
-            </Popup>
-          )}
+  const [clickLocation, setClickLocation] = React.useState(null);
 
-</MapContainer>
+  const MapEvents = () => {
+    useMapEvents({
+      click(e) {
+        setClickLocation(e);
+        setActiveRack(null);
+        console.log(e.latlng.lat);
+        console.log(e.latlng.lng);
+      },
+    });
+    return null;
+  };
+
+  // var nameValue = document.getElementById("rackName").value;
+  // console.log(nameValue);
+
+  return (
+    <MapContainer
+      center={[38.71666369749177, -75.07651347222257]}
+      zoom={15}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {racks.map((rack) => (
+        <Marker
+          key={rack.id}
+          position={[rack.latitude, rack.longitude]}
+          eventHandlers={{
+            click: (e2) => {
+              setActiveRack(rack);
+              setImage(rack.image);
+              setClickLocation(null);
+            },
+          }}
+          icon={icon}
+        />
+      ))}
+
+      <MapEvents />
+
+      {clickLocation && (
+        <Popup
+          position={[clickLocation.latlng.lat, clickLocation.latlng.lng]}
+          onClose={() => {
+            setActiveRack(null);
+            setClickLocation(null);
+          }}
+        >
+          <div>
+            <h2>RACK IT?</h2>
+            <form>
+              <label htmlFor="rackName">Rack Name:</label>
+              <br />
+              <input type="text" id="rackName" name="rackName" />
+              <br />
+            </form>
+            <button />
+          </div>
+        </Popup>
+      )}
+
+      {activeRack && (
+        <Popup
+          position={[activeRack.latitude, activeRack.longitude]}
+          onClose={() => {
+            setActiveRack(null);
+            setClickLocation(null);
+          }}
+        >
+          <div>
+            <h2>{activeRack.name}</h2>
+            <img src={imgSrc} className="rackImage" />
+          </div>
+        </Popup>
+      )}
+    </MapContainer>
   );
 }
